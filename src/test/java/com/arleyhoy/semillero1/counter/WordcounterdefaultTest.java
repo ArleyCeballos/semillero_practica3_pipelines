d2c0c8ba5cde6a142d36b211d0efdcedaf3296cc
arleydevco <wceballosh@devco.com.co>
2022-09-16 05:19:34 -0500

    re crea el Worldcounterdefaulttest para hacer el test

--- src/test/java/com/arleyhoy/semillero1/counter/WordcounterdefaultTest.java
+++ src/test/java/com/arleyhoy/semillero1/counter/WordcounterdefaultTest.java
@@ -1,11 +1,23 @@
 package com.arleyhoy.semillero1.counter;
-
+import com.arleyhoy.semillero1.counter.model.Summary;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class WordcounterdefaultTest {
     @Test
-    void mustReturnCorrectSummary(){
-        Assertions.fail();
+    void mustReturnCorrectSummary() throws IOException {
+        Path resourceDirectory = Paths.get("src","test","resources");
+        String absolutePath=resourceDirectory.toFile().getAbsolutePath();
+        WordCounter wordCounter =new Wordcounterdefault();
+
+        Summary summary = wordCounter.process(absolutePath + File.separator +"test.txt");
+
+        Assertions.assertEquals(2,summary.getNumberOfLines());
+        Assertions.assertEquals(5, summary.getNumberOfWords());
     }
 }
