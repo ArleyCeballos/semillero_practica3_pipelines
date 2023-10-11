513449b52552c9d6875a0bebb1b0c6b1f60871ff
Arley <wilmerarleyd@gmail.com>
2023-10-11 02:12:11 -0500

    add a code smell file

---
+++ src/main/java/com/arleyhoy/semillero1/counter/smell-test.java
@@ -0,0 +1,36 @@
+
+
+public static void main(String args[]) throws Exception { // Noncompliant
+    int x = 1;
+    int y=0;
+    if (x == 0) {
+        y= 1
+      } else if (x == 1) {
+        y = 2;  
+      }
+      else if (true=true){
+        if (true=true){
+          System.out.println("amen");
+          if (true=true){
+            System.out.println("amen");
+            if (true=true){
+              System.out.println("amen");
+              if (true=true){
+                System.out.println("amen");
+                if (true=true){
+                  System.out.println("amen");
+                  if (true=true){
+                    System.out.println("amen");
+                  }    
+                }    
+              }    
+            }    
+          }    
+        }    
+      }
+
+
+
+  }
+
+
