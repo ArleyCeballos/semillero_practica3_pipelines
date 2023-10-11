a72f5eb65afc3e69c28370d4e2d587139246c43a
Arley <wilmerarleyd@gmail.com>
2023-10-11 02:24:27 -0500

    smell fiile correction v.3.0

--- src/main/java/com/arleyhoy/semillero1/counter/smell-test.java
+++ src/main/java/com/arleyhoy/semillero1/counter/smell-test.java
@@ -1,36 +1,47 @@
+public class BadCode {
 
-
-public static void main(String args[]) throws Exception { // Noncompliant
-    int x = 1;
-    int y=0;
-    if (x == 0) {
-        y= 1;
-      } else if (x == 1) {
-        y = 2;  
-      }
-      else if (true==true){
-        if (true==true){
-          System.out.println("amen");
-          if (true==true){
-            System.out.println("amen");
-            if (true==true){
-              System.out.println("amen");
-              if (true==true){
-                System.out.println("amen");
-                if (true==true){
-                  System.out.println("amen");
-                  if (true==true){
-                    System.out.println("amen");
-                  }    
-                }    
-              }    
-            }    
-          }    
-        }    
-      }
-
-
-
+  public static void main(String[] args) {
+      int a = 5;
+      int b = 10;
+      badMethod(a, b);
   }
 
+  public static void badMethod(int x, int y) {
+      if (x > y) {
+          System.out.println("x is greater");
+          if (x < 100) {
+              if (y < 50) {
+                  System.out.println("y is small");
+              } else if (y > 100) {
+                  System.out.println("y is big");
+              } else {
+                  if (x + y > 100) {
+                      System.out.println("sum is big");
+                  } else {
+                      System.out.println("sum is small");
+                  }
+              }
+          } else {
+              System.out.println("x is big");
+          }
+      } else if (x < y) {
+          System.out.println("y is greater");
+      } else {
+          System.out.println("x and y are equal");
+          for (int i = 0; i < 10; i++) {
+              for (int j = 0; j < 10; j++) {
+                  System.out.println(i * j);
+              }
+          }
+      }
+      badMethod2();
+  }
 
+  public static void badMethod2() {
+      int a = 5;
+      int b = 10;
+      int c = a + b;
+      System.out.println(c);
+      System.out.println("Bad method finished");
+  }
+}
