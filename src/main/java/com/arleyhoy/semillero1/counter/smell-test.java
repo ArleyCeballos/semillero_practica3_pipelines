7d962ac3a5b3ed8e1e0a614b078a19ed885ade88
Arley <wilmerarleyd@gmail.com>
2023-10-11 02:18:01 -0500

    smell file correction v 2.0

--- src/main/java/com/arleyhoy/semillero1/counter/smell-test.java
+++ src/main/java/com/arleyhoy/semillero1/counter/smell-test.java
@@ -4,7 +4,7 @@ public static void main(String args[]) throws Exception { // Noncompliant
     int x = 1;
     int y=0;
     if (x == 0) {
-        y= 1
+        y= 1;
       } else if (x == 1) {
         y = 2;  
       }
